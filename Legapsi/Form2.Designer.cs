c221bba9a5873c2887dcc5ba2fe2737604133dfd
Zyrille Karlo Legaspi <2024-2-02003@lpunetwork.edu.ph>
2026-01-30 19:19:49 +0800

    eto na yon?

---
+++ Legapsi/Form2.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace OOP_LEGASPI_CPE201
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            groupBox1 = new GroupBox();
+            label5 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            maskedTextBox4 = new MaskedTextBox();
+            label8 = new Label();
+            maskedTextBox2 = new MaskedTextBox();
+            label6 = new Label();
+            maskedTextBox3 = new MaskedTextBox();
+            label7 = new Label();
+            maskedTextBox5 = new MaskedTextBox();
+            label9 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Algerian", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(81, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(354, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Pizza ni Karlo Legaspi";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(40, 92);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 1;
+            label2.Text = "label12";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(203, 92);
+            label3.Name = "label3";
+            label3.Size = new Size(132, 15);
+            label3.TabIndex = 2;
+            label3.Text = "By: Annabella C. Doctor";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(379, 92);
+            label4.Name = "label4";
+            label4.Size = new Size(74, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Terminal #21";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(maskedTextBox5);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(maskedTextBox2);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(maskedTextBox3);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(maskedTextBox4);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(maskedTextBox1);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Location = new Point(54, 120);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(381, 175);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(17, 19);
+            label5.Name = "label5";
+            label5.Size = new Size(99, 15);
+            label5.TabIndex = 5;
+            label5.Text = "Name of an item:";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(131, 16);
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(244, 23);
+            maskedTextBox1.TabIndex = 6;
+            // 
+            // maskedTextBox4
+            // 
+            maskedTextBox4.Location = new Point(131, 45);
+            maskedTextBox4.Name = "maskedTextBox4";
+            maskedTextBox4.Size = new Size(145, 23);
+            maskedTextBox4.TabIndex = 12;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(17, 48);
+            label8.Name = "label8";
+            label8.Size = new Size(99, 15);
+            label8.TabIndex = 11;
+            label8.Text = "Name of an item:";
+            label8.Click += label8_Click;
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new Point(131, 103);
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new Size(145, 23);
+            maskedTextBox2.TabIndex = 16;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(17, 106);
+            label6.Name = "label6";
+            label6.Size = new Size(99, 15);
+            label6.TabIndex = 15;
+            label6.Text = "Name of an item:";
+            // 
+            // maskedTextBox3
+            // 
+            maskedTextBox3.Location = new Point(131, 74);
+            maskedTextBox3.Name = "maskedTextBox3";
+            maskedTextBox3.Size = new Size(145, 23);
+            maskedTextBox3.TabIndex = 14;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(17, 77);
+            label7.Name = "label7";
+            label7.Size = new Size(99, 15);
+            label7.TabIndex = 13;
+            label7.Text = "Name of an item:";
+            // 
+            // maskedTextBox5
+            // 
+            maskedTextBox5.Location = new Point(131, 132);
+            maskedTextBox5.Name = "maskedTextBox5";
+            maskedTextBox5.Size = new Size(145, 23);
+            maskedTextBox5.TabIndex = 18;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(17, 135);
+            label9.Name = "label9";
+            label9.Size = new Size(99, 15);
+            label9.TabIndex = 17;
+            label9.Text = "Name of an item:";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveBorder;
+            button1.Location = new Point(282, 45);
+            button1.Name = "button1";
+            button1.Size = new Size(93, 23);
+            button1.TabIndex = 5;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveBorder;
+            button2.Location = new Point(282, 74);
+            button2.Name = "button2";
+            button2.Size = new Size(93, 23);
+            button2.TabIndex = 19;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveBorder;
+            button3.Location = new Point(282, 132);
+            button3.Name = "button3";
+            button3.Size = new Size(93, 23);
+            button3.TabIndex = 21;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveBorder;
+            button4.Location = new Point(282, 103);
+            button4.Name = "button4";
+            button4.Size = new Size(93, 23);
+            button4.TabIndex = 20;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(1478, 646);
+            Controls.Add(groupBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form2";
+            Load += Form2_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private GroupBox groupBox1;
+        private MaskedTextBox maskedTextBox4;
+        private Label label8;
+        private MaskedTextBox maskedTextBox1;
+        private Label label5;
+        private MaskedTextBox maskedTextBox5;
+        private Label label9;
+        private MaskedTextBox maskedTextBox2;
+        private Label label6;
+        private MaskedTextBox maskedTextBox3;
+        private Label label7;
+        private Button button2;
+        private Button button1;
+        private Button button3;
+        private Button button4;
+    }
+}
